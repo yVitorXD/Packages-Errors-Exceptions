d6f01689bd2dba297fb21b5c9d5d1566aa48997a
yVitorXD <vitorxdxx@gmail.com>
2023-08-17 22:40:46 -0300

    IDAO, GenericDAO

---
+++ src/unidade3/persistencia/IDAO.java
@@ -0,0 +1,10 @@
+package unidade3.persistencia;
+
+public interface IDAO<T> {
+
+	public void create(T entidade);
+	public void read(T entidade);
+	public void update(T entidade);
+	public void delete(T entidade);
+	
+}
